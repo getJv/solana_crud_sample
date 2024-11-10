b2aa06d3bad6f34ed7c324b727b420871c432da9
Jhonatan Morais <jhonatanvinicius@gmail.com>
2024-11-10 10:53:48 -0300

    chore: Add journal crud sample

---
+++ README.md
@@ -0,0 +1,19 @@
+# Solana CRUD 
+
+tech stack: React + TypeScript + Vite + Anchor
+
+this is a Solana App with a CRUD operation of journals... 
+You can find the official tutorial here:
+ - https://solana.com/developers/guides/dapps/journal
+ - https://www.youtube.com/watch?v=cW-3jGWq1IE
+
+## How to Deploy
+ - Sync the Keys `anchor keys sync`
+ - build `cargo build-sbf && anchor build`
+ - then deploy on devnet `anchor deploy --provider.cluster devnet`
+
+## Dev run: 
+ - yarn dev
+ - hot to http://localhost:5173/journal
+
+
---
+++ src/components/journal/journal-feature.tsx
@@ -0,0 +1,39 @@
+import { useWallet } from '@solana/wallet-adapter-react'
+import { ExplorerLink } from '../cluster/cluster-ui'
+import { WalletButton } from '../solana/solana-provider'
+import { AppHero, ellipsify } from '../ui/ui-layout'
+import { useJournalProgram } from './journal-data-access'
+import { JournalCreate, JournalList } from './journal-ui'
+
+export default function JournalFeature() {
+  const { publicKey } = useWallet();
+  const { programId } = useJournalProgram();
+
+  return publicKey ? (
+    <div>
+      <AppHero
+        title="My Solana Journal"
+        subtitle={
+          'Create your journal here!'
+        }
+      >
+        <p className="mb-6">
+          <ExplorerLink
+            path={`account/${programId}`}
+            label={ellipsify(programId.toString())}
+          />
+        </p>
+        <JournalCreate />
+      </AppHero>
+      <JournalList />
+    </div>
+  ) : (
+    <div className="max-w-4xl mx-auto">
+      <div className="hero py-[64px]">
+        <div className="hero-content text-center">
+          <WalletButton />
+        </div>
+      </div>
+    </div>
+  );
+}
